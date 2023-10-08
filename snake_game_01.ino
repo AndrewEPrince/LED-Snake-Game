d3c309f1ac21255cbd0956a8d118964fb29d369e
Adam Murray <63177370+Adiaslow@users.noreply.github.com>
2023-10-07 18:50:54 -0700

    The Snake Groweth
    The snake now grows when it eats an apple.

--- snake_game_01.ino
+++ snake_game_01.ino
@@ -9,11 +9,10 @@ constexpr int RIGHT_BUTTON_PIN = 13;
 constexpr int NUM_LED_STRIPS = 8;
 constexpr int LEDS_PER_STRIP = 8;
 constexpr int BRIGHTNESS = 255;
+constexpr int TOTAL_LEDS = NUM_LED_STRIPS * LEDS_PER_STRIP;
 
 Adafruit_NeoPixel strip(NUM_LED_STRIPS * LEDS_PER_STRIP, LED_PIN, NEO_GRB + NEO_KHZ800);
 
-int TOTAL_LEDS = NUM_LED_STRIPS * LEDS_PER_STRIP;
-int frame = 0;
 int wait = 500;
 
 int left_button_state = 0;
@@ -33,10 +32,27 @@ enum class Direction {
 
 class Apple;
 
+class Array {
+private:
+    int data[TOTAL_LEDS];
+    int size;
+
+public:
+    Array() : size(0) {}
+
+    void push_back(int value);
+    void pop_back();
+    int get_size() const;
+    int operator[](int index) const;
+};
+
 class Snake {
 public:
     int position[2];
     int velocity[2];
+  	int length = 3;
+    Array segments_x;
+  	Array segments_y;
     Direction current_direction;
     uint32_t color;
     bool is_switched = false;
@@ -58,6 +74,7 @@ public:
 };
 
 int position_to_i(const int position[2]);
+int xy_to_i(const int x, const int y);
 
 Snake snake1;
 Apple apple1;
@@ -79,8 +96,8 @@ void setup() {
     apple1.show();
 
     snake1.color = strip.Color(255, 0, 0);
-    snake1.position[0] = 0;
-    snake1.position[1] = 0;
+    snake1.position[0] = 2;
+    snake1.position[1] = 2;
     snake1.velocity[0] = 1;
     snake1.velocity[1] = 0;
     snake1.current_direction = Direction::RIGHT;
@@ -98,8 +115,6 @@ void loop() {
     Serial.println(snake1.velocity[1]);
     check_inputs(snake1);
     snake1.update(apple1);
-    snake1.show();
-    frame += 1;
     delay(wait);
 }
 
@@ -121,6 +136,39 @@ void check_inputs(Snake &snake1) {
     if (right_button_state == HIGH) {
         snake1.change_direction(Direction::RIGHT);
     }
+}
+
+void Array::push_back(int value) {
+        if (size < TOTAL_LEDS) {
+            data[size++] = value;
+        } else {
+            Serial.println("Array is full. Cannot push more elements.");
+        }
+    }
+
+void Array::pop_back() {
+    if (size > 0) {
+        // Shift all elements down by one position
+        for (int i = 0; i < size - 1; i++) {
+            data[i] = data[i + 1];
+        }
+        // Clear the last element
+        data[--size] = 0;
+    } else {
+        Serial.println("Array is empty. Cannot pop elements.");
+    }
+}
+int Array::get_size() const {
+  return size;
+}
+
+int Array::operator[](int index) const {
+  if (index >= 0 && index < size) {
+    return data[index];
+  } else {
+    Serial.println("Index out of bounds");
+    //Serial.println(index);
+  }
 }
 
 void Snake::update(Apple apple1) {
@@ -145,11 +193,22 @@ void Snake::update(Apple apple1) {
     else if (is_switched) {
         position[0] = (((position[0] - velocity[0]) % LEDS_PER_STRIP) + LEDS_PER_STRIP) % LEDS_PER_STRIP;
     }
-
+	
+  	segments_x.push_back(position[0]);
+  	segments_y.push_back(position[1]);
+  
     if (position[0] == apple1.position[0] and position[1] == apple1.position[1]) {
         apple1.spawn_random();
       	apple1.show();
+      	length++;
     }
+    else if (length < segments_x.get_size()) {
+        strip.setPixelColor(xy_to_i(segments_x[0], segments_y[0]), strip.Color(0,0,0));
+		segments_x.pop_back();
+      	segments_y.pop_back();
+        Serial.println("POPPED");
+    }
+    snake1.show();
 }
 
 void Snake::change_direction(Direction direction) {
@@ -186,9 +245,16 @@ void Snake::change_direction(Direction direction) {
 }
 
 void Snake::show() {
-    strip.setPixelColor(position_to_i(position), color);
+  for (int i = 0; i < length; i++) {
+    strip.setPixelColor(xy_to_i(segments_x[i], segments_y[i]), color);
+    strip.show();
+  }
+  
+  /*
+  strip.setPixelColor(position_to_i(position), color);
     strip.setPixelColor(position_to_i(prev_position), strip.Color(0, 0, 0));
     strip.show();
+  */
 }
 
 void Apple::spawn_random() {
@@ -203,4 +269,8 @@ void Apple::show() {
 
 int position_to_i(const int position[2]) {
     return position[0] + NUM_LED_STRIPS * position[1];
+}
+
+int xy_to_i(const int x, const int y) {
+    return x + NUM_LED_STRIPS * y;
 }
