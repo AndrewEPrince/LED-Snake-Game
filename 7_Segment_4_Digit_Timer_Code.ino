18a0ee45e9371800fd24bb57e4d94a132277be0c
bryanberry590 <144753561+bryanberry590@users.noreply.github.com>
2023-10-17 18:43:23 -0700

    7_Segment_4_Digit_Timer_Code.ino
    Classes for Timer and Score, the timer works and the score prints to the 7-segment 4-digit timer board.

--- 7_Segment_4_Digit_Timer_Code.ino
+++ 7_Segment_4_Digit_Timer_Code.ino
@@ -1,276 +1,316 @@
-#include <SevSeg.h>
+class SevenSegment {
+  private:
+    int pinA = 11;
+    int pinB = 7;
+    int pinC = 4;
+    int pinD = 2;
+    int pinE = 1;
+    int pinF = 10;
+    int pinG = 5;
+    
+    int D1 = 12;
+    int D2 = 9;
+    int D3 = 8;
+    int D4 = 6;
 
-//Declares Variables
-int pinA = 11; //
-int pinB = 7; //
-int pinC = 4; //
-int pinD = 2; //
-int pinE = 1; //
-int pinF = 10; //
-int pinG = 5; //
-int pinDP = 3; //
-int D1 = 12; //
-int D2 = 9; //
-int D3 = 8; //
-int D4 = 6; //
+  public:
+    void setup();
+    void showZero();
+    void showOne();
+    void showTwo();
+    void showThree();
+    void showFour();
+    void showFive();
+    void showSix();
+    void showSeven();
+    void showEight();
+    void showNine();
+    void turnOffAllSegments();
+    void selectThirdDigit();
+    void selectFirstDigit();
+    void selectSecondDigit();
+    void selectFourthDigit();
+    void selectAll4Digits();
+    void displayDigit(int value);
+};
 
-int tens;
-int ones;
-int i;
-
-unsigned long previousMillis = 0;
-unsigned long interval = 1000UL;
-unsigned long seconds = 10;
-
-int dt = 1000;
+class ShowScore {
+  public:
+    void showScore(SevenSegment sSegment, int value);  
+};
 
 class Timer {
 
   private: 
-
-
-    int tens;
-    int ones;
+    int tens = 1;
+    int ones = 0;
+    unsigned long previousMillis = 0;
+    unsigned long interval = 1000UL;
+    unsigned long seconds = 10;
 
   public:
+    boolean isTimeOver = false;
     void restart();
-    
-
-
+    void updateTimeByFrame();
+    void showTime(SevenSegment sSegment);
 };
 
-//void zero(void), one(void), two(void), three(void), four(void), five(void), six(void), seven(void), eight(void), nine(void);
-
-//void (*segmentDisplay[]) = {zero, one, two, three, four, five, six, seven, eight, nine};
+SevenSegment sevenSegment;
+Timer timer;
+ShowScore sScore;
 
 void setup() {//set all segments & digits as outputs
-
-pinMode(pinA, OUTPUT);
-pinMode(pinB, OUTPUT);
-pinMode(pinC, OUTPUT);
-pinMode(pinD, OUTPUT);
-pinMode(pinE, OUTPUT);
-pinMode(pinF, OUTPUT);
-pinMode(pinG, OUTPUT);
-pinMode(pinDP, OUTPUT);
-pinMode(D1, OUTPUT);
-pinMode(D2, OUTPUT);
-pinMode(D3, OUTPUT);
-pinMode(D4, OUTPUT);
-
+  sevenSegment.setup();
 }
 
 void loop() {
+  timer.updateTimeByFrame();
   
-  if ((millis() - previousMillis) > interval) {
-      previousMillis += interval;
-      seconds--;
-      
+  timer.showTime(sevenSegment);
+  sScore.showScore(sevenSegment, 32);
 
-  if (seconds <= 0) {
-    ones = 0;
-    tens = 0;
-
-  } else {
-  ones = seconds % 10;
-  tens = seconds / 10;
-
+  if (timer.isTimeOver) { // GAMEOVER
+    timer.restart();
   }
-
-  }
-
-  
-
-  firstDigit();
-  CounterWithDisplay(tens);
-
-  delay(5);
-
-  secondDigit();
-  CounterWithDisplay(ones);
-
-  delay(5);
-
 }
 
-void CounterWithDisplay(int value) {
-  if (value == 0) {
-    zero();
-  } else if (value == 1) {
-    one();
-  } else if (value == 2) {
-    two();
-  } else if (value == 3) {
-    three();
-  } else if (value == 4) {
-    four();
-  } else if (value == 5) {
-    five();
-  } else if (value == 6) {
-    six();
-  } else if (value == 7) {
-    seven();
-  } else if (value == 8) {
-    eight();
-  } else if (value == 9) {
-    nine();
-  } else {
-  }
+void ShowScore::showScore(SevenSegment sSegment, int value) {
+  int dNumber = 2;
+  int ones = value % 10;
+  int tens = value / 10;
+  
+  sSegment.selectThirdDigit();
+  sSegment.displayDigit(tens);
+
+  delay(dNumber);
+
+  sSegment.selectFourthDigit();
+  sSegment.displayDigit(ones);
+
+  delay(dNumber);
 }
 
 void Timer::restart() {
-
-  seconds = 99;
-
+  seconds = 10;
+  isTimeOver = false;
+  ones = 1;
+  tens = 0;
 }
 
-//functions representing numbers 0-9
-void zero() {
-digitalWrite(pinA, HIGH);
-digitalWrite(pinB, HIGH);
-digitalWrite(pinC, HIGH);
-digitalWrite(pinD, HIGH);
-digitalWrite(pinE, HIGH);
-digitalWrite(pinF, HIGH);
-digitalWrite(pinG, LOW);
-  
-  }
+void Timer::updateTimeByFrame() {
+  if ((millis() - previousMillis) > interval) {
+      previousMillis += interval;
+      seconds--;
 
-  void one(){
-digitalWrite(pinA, LOW);
-digitalWrite(pinB, HIGH);
-digitalWrite(pinC, HIGH);
-digitalWrite(pinD, LOW);
-digitalWrite(pinE, LOW);
-digitalWrite(pinF, LOW);
-digitalWrite(pinG, LOW);
+      if (seconds > 0) {
+        ones = seconds % 10;
+        tens = seconds / 10;
+      } else {
+        isTimeOver = true;
+        ones = 0;
+        tens = 0;
+      }
+  }
 }
 
-void two(){
-digitalWrite(pinA, HIGH);
-digitalWrite(pinB, HIGH);
-digitalWrite(pinC, LOW);
-digitalWrite(pinD, HIGH);
-digitalWrite(pinE, HIGH);
-digitalWrite(pinF, LOW);
-digitalWrite(pinG, HIGH);
-  }
+void Timer::showTime(SevenSegment sSegment) {
+  int dNumber = 2;
   
-  void three(){
-digitalWrite(pinA, HIGH);
-digitalWrite(pinB, HIGH);
-digitalWrite(pinC, HIGH);
-digitalWrite(pinD, HIGH);
-digitalWrite(pinE, LOW);
-digitalWrite(pinF, LOW);
-digitalWrite(pinG, HIGH);
-    }
+  sevenSegment.selectFirstDigit();
+  sevenSegment.displayDigit(tens);
+
+  delay(dNumber);
+
+  sevenSegment.selectSecondDigit();
+  sevenSegment.displayDigit(ones);
+
+  delay(dNumber);
+}
+
+// SEVEN SEGMENTS METHODS
+void SevenSegment::setup() {
+  pinMode(pinA, OUTPUT);
+  pinMode(pinB, OUTPUT);
+  pinMode(pinC, OUTPUT);
+  pinMode(pinD, OUTPUT);
+  pinMode(pinE, OUTPUT);
+  pinMode(pinF, OUTPUT);
+  pinMode(pinG, OUTPUT);
+  pinMode(pinD, OUTPUT);
+  
+  pinMode(D1, OUTPUT);
+  pinMode(D2, OUTPUT);
+  pinMode(D3, OUTPUT);
+  pinMode(D4, OUTPUT);
+}
+
+void SevenSegment::displayDigit(int value) {
+  if (value == 0) {
+    sevenSegment.showZero();
+  } else if (value == 1) {
+    sevenSegment.showOne();
+  } else if (value == 2) {
+    sevenSegment.showTwo();
+  } else if (value == 3) {
+    sevenSegment.showThree();
+  } else if (value == 4) {
+    sevenSegment.showFour();
+  } else if (value == 5) {
+    sevenSegment.showFive();
+  } else if (value == 6) {
+    sevenSegment.showSix();
+  } else if (value == 7) {
+    sevenSegment.showSeven();
+  } else if (value == 8) {
+    sevenSegment.showEight();
+  } else if (value == 9) {
+    sevenSegment.showNine();
+  } else {
+  }
+}
+
+void SevenSegment::showZero() {
+  digitalWrite(pinA, HIGH);
+  digitalWrite(pinB, HIGH);
+  digitalWrite(pinC, HIGH);
+  digitalWrite(pinD, HIGH);
+  digitalWrite(pinE, HIGH);
+  digitalWrite(pinF, HIGH);
+  digitalWrite(pinG, LOW);
+}
+
+void SevenSegment::showOne(){
+  digitalWrite(pinA, LOW);
+  digitalWrite(pinB, HIGH);
+  digitalWrite(pinC, HIGH);
+  digitalWrite(pinD, LOW);
+  digitalWrite(pinE, LOW);
+  digitalWrite(pinF, LOW);
+  digitalWrite(pinG, LOW);
+}
+void SevenSegment::showTwo(){
+  digitalWrite(pinA, HIGH);
+  digitalWrite(pinB, HIGH);
+  digitalWrite(pinC, LOW);
+  digitalWrite(pinD, HIGH);
+  digitalWrite(pinE, HIGH);
+  digitalWrite(pinF, LOW);
+  digitalWrite(pinG, HIGH);
+}
+  
+void SevenSegment::showThree(){
+  digitalWrite(pinA, HIGH);
+  digitalWrite(pinB, HIGH);
+  digitalWrite(pinC, HIGH);
+  digitalWrite(pinD, HIGH);
+  digitalWrite(pinE, LOW);
+  digitalWrite(pinF, LOW);
+  digitalWrite(pinG, HIGH);
+}
     
-  void four(){
-digitalWrite(pinA, LOW);
-digitalWrite(pinB, HIGH);
-digitalWrite(pinC, HIGH);
-digitalWrite(pinD, LOW);
-digitalWrite(pinE, LOW);
-digitalWrite(pinF, HIGH);
-digitalWrite(pinG, HIGH);
-    }
+void SevenSegment::showFour(){
+  digitalWrite(pinA, LOW);
+  digitalWrite(pinB, HIGH);
+  digitalWrite(pinC, HIGH);
+  digitalWrite(pinD, LOW);
+  digitalWrite(pinE, LOW);
+  digitalWrite(pinF, HIGH);
+  digitalWrite(pinG, HIGH);
+}
     
-  void five(){
-digitalWrite(pinA, HIGH);
-digitalWrite(pinB, LOW);
-digitalWrite(pinC, HIGH);
-digitalWrite(pinD, HIGH);
-digitalWrite(pinE, LOW);
-digitalWrite(pinF, HIGH);
-digitalWrite(pinG, HIGH);
-    }
+void SevenSegment::showFive(){
+  digitalWrite(pinA, HIGH);
+  digitalWrite(pinB, LOW);
+  digitalWrite(pinC, HIGH);
+  digitalWrite(pinD, HIGH);
+  digitalWrite(pinE, LOW);
+  digitalWrite(pinF, HIGH);
+  digitalWrite(pinG, HIGH);
+}
     
-  void six(){
-digitalWrite(pinA, LOW);
-digitalWrite(pinA, HIGH);
-digitalWrite(pinB, LOW);
-digitalWrite(pinC, HIGH);
-digitalWrite(pinD, HIGH);
-digitalWrite(pinE, HIGH);
-digitalWrite(pinF, HIGH);
-digitalWrite(pinG, HIGH);
-    }
+void SevenSegment::showSix(){
+  digitalWrite(pinA, LOW);
+  digitalWrite(pinA, HIGH);
+  digitalWrite(pinB, LOW);
+  digitalWrite(pinC, HIGH);
+  digitalWrite(pinD, HIGH);
+  digitalWrite(pinE, HIGH);
+  digitalWrite(pinF, HIGH);
+  digitalWrite(pinG, HIGH);
+}
     
-  void seven(){
-digitalWrite(pinA, HIGH);
-digitalWrite(pinB, HIGH);
-digitalWrite(pinC, HIGH);
-digitalWrite(pinD, LOW);
-digitalWrite(pinE, LOW);
-digitalWrite(pinF, LOW);
-digitalWrite(pinG, LOW);
-    }
+void SevenSegment::showSeven(){
+  digitalWrite(pinA, HIGH);
+  digitalWrite(pinB, HIGH);
+  digitalWrite(pinC, HIGH);
+  digitalWrite(pinD, LOW);
+  digitalWrite(pinE, LOW);
+  digitalWrite(pinF, LOW);
+  digitalWrite(pinG, LOW);
+}
     
-  void eight(){
-digitalWrite(pinA, HIGH);
-digitalWrite(pinB, HIGH);
-digitalWrite(pinC, HIGH);
-digitalWrite(pinD, HIGH);
-digitalWrite(pinE, HIGH);
-digitalWrite(pinF, HIGH);
-digitalWrite(pinG, HIGH);
-    }
+void SevenSegment::showEight(){
+  digitalWrite(pinA, HIGH);
+  digitalWrite(pinB, HIGH);
+  digitalWrite(pinC, HIGH);
+  digitalWrite(pinD, HIGH);
+  digitalWrite(pinE, HIGH);
+  digitalWrite(pinF, HIGH);
+  digitalWrite(pinG, HIGH);
+}
     
-  void nine(){
-digitalWrite(pinA, HIGH);
-digitalWrite(pinB, HIGH);
-digitalWrite(pinC, HIGH);
-digitalWrite(pinD, HIGH);
-digitalWrite(pinE, LOW);
-digitalWrite(pinF, HIGH);
-digitalWrite(pinG, HIGH);
-    }
+void SevenSegment::showNine(){
+  digitalWrite(pinA, HIGH);
+  digitalWrite(pinB, HIGH);
+  digitalWrite(pinC, HIGH);
+  digitalWrite(pinD, HIGH);
+  digitalWrite(pinE, LOW);
+  digitalWrite(pinF, HIGH);
+  digitalWrite(pinG, HIGH);
+}
 
 
-  void turnOffAllSegments(){
-digitalWrite(pinA, LOW);
-digitalWrite(pinB, LOW);
-digitalWrite(pinC, LOW);
-digitalWrite(pinD, LOW);
-digitalWrite(pinE, LOW);
-digitalWrite(pinF, LOW);
-digitalWrite(pinG, LOW);
-          }
+void SevenSegment::turnOffAllSegments(){
+  digitalWrite(pinA, LOW);
+  digitalWrite(pinB, LOW);
+  digitalWrite(pinC, LOW);
+  digitalWrite(pinD, LOW);
+  digitalWrite(pinE, LOW);
+  digitalWrite(pinF, LOW);
+  digitalWrite(pinG, LOW);
+}
 
-//functions to select individual digits on the display
-
-  void thirdDigit(){
+void SevenSegment::selectThirdDigit(){
     digitalWrite(D1, HIGH);
     digitalWrite(D2, HIGH);
     digitalWrite(D3, LOW);
     digitalWrite(D4, HIGH);
 }
       
-  void firstDigit(){
+void SevenSegment::selectFirstDigit(){
     digitalWrite(D1, LOW);
     digitalWrite(D2, HIGH);
     digitalWrite(D3, HIGH);
     digitalWrite(D4, HIGH);
-      }
+}
       
-  void secondDigit(){
+void SevenSegment::selectSecondDigit(){
     digitalWrite(D1, HIGH);
     digitalWrite(D2, LOW);
     digitalWrite(D3, HIGH);
     digitalWrite(D4, HIGH);
-      }
+}
       
-  void fourthDigit(){
+void SevenSegment::selectFourthDigit(){
     digitalWrite(D1, HIGH);
     digitalWrite(D2, HIGH);
     digitalWrite(D3, HIGH);
     digitalWrite(D4, LOW);
-      }
+}
 
-  void all4Digits(){
+void SevenSegment::selectAll4Digits(){
     digitalWrite(D1, LOW);
     digitalWrite(D2, LOW);
     digitalWrite(D3, LOW);
     digitalWrite(D4, LOW);
-        }
+}
