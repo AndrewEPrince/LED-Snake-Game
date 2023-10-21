72b7433426eb6505a8e9e53c9048ceac5102166b
Caleb Clements <79200015+calebwclem@users.noreply.github.com>
2023-10-21 13:58:03 -0700

    Create 7segment.h

---
+++ main/7segment.h
@@ -0,0 +1,46 @@
+class SevenSegment {
+  private:
+
+  //Create a private class and set up pins
+  //Sets up the pins for segments
+    int pinA = 11;
+    int pinB = 7;
+    int pinC = 4;
+    int pinD = 2;
+    int pinE = 1;
+    int pinF = 10;
+    int pinG = 5;
+    
+  //Sets up the pins for digits
+    int D1 = 12;
+    int D2 = 9;
+    int D3 = 8;
+    int D4 = 6;
+
+//Sets up all of the segments...
+  public:
+    void setup();
+    void showZero();
+    void showOne();
+    void showTwo();
+    void showThree();
+    void showFour();
+    void showFive();
+    void showSix();
+    void showSeven();
+    void showEight();
+    void showNine();
+    void AllturnOffSegments();
+    void selectThirdDigit();
+    void selectFirstDigit();
+    void selectSecondDigit();
+    void selectFourthDigit();
+    void selectAll4Digits();
+    void displayDigit(int value);
+};
+
+//This shows the score from the value passed to it.
+class ShowScore {
+  public:
+    void showScore(SevenSegment sSegment, int value);  
+};
