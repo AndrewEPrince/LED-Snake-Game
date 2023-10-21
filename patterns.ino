6112fe78bc38a4afb30f7c5eb0f1797d78097b4c
Adam Murray <63177370+Adiaslow@users.noreply.github.com>
2023-10-21 09:33:23 -0700

    Update patterns.ino

--- patterns.ino
+++ patterns.ino
@@ -22,16 +22,31 @@ void raindow_y(const int offset) {
     strip.show();
 }
 
-void rainbow_waterfall_x(const int loops) {
+void rainbow_waterfall_x(const int loops, const int wait) {
     for (int i = 0; i < loops; i++) {
         raindow_x(-i);
-        delay(100);
+        delay(wait);
     }
 }
 
-void rainbow_waterfall_y(const int loops) {
+void rainbow_waterfall_y(const int loops, const int wait) {
     for (int i = 0; i < loops; i++) {
         raindow_y(-i);
-        delay(100);
+        delay(wait);
+    }
+}
+
+void random_hsv(const int loops, const int wait) {
+  int x;
+  int y;
+  for (int i = 0; i < loops; i++) {
+        strip.clear();
+    for (int i = 0; i < TOTAL_LEDS; i++) {
+        y = i_to_y(i);
+        x = i_to_x(i, y);
+        strip.setPixelColor(i, strip.ColorHSV(rand() % 65535, 255, 255));
+    }
+    strip.show();
+        delay(wait);
     }
 }
