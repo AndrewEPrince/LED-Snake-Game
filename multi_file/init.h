85b8c64d4d12afd37f7f0bc034004af940538c27
YousifAlkhalaf <58121901+YousifAlkhalaf@users.noreply.github.com>
2023-10-28 09:16:09 -0700

    Edited constants

--- multi_file/init.h
+++ multi_file/init.h
@@ -1,11 +1,11 @@
 #ifndef INIT_H
 #define INIT_H
 
-constexpr int LED_PIN = 2;
-constexpr int LEFT_BUTTON_PIN = 10;
-constexpr int UP_BUTTON_PIN = 11;
-constexpr int DOWN_BUTTON_PIN = 12;
-constexpr int RIGHT_BUTTON_PIN = 13;
+constexpr int LED_PIN = 3;
+constexpr int LEFT_BUTTON_PIN = 12;
+constexpr int UP_BUTTON_PIN = 13;
+constexpr int DOWN_BUTTON_PIN = 10;
+constexpr int RIGHT_BUTTON_PIN = 11;
 
 constexpr int NUM_LED_STRIPS = 16;
 constexpr int LEDS_PER_STRIP = 32;
